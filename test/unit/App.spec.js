f9848fe1025ba66a4daeb43fa48027a46b07d622
Tommy Wilczk <tommywilczek@utexas.edu>
2019-07-14 09:30:46 -0500

    set up enzyme

---
+++ test/unit/App.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import{ mount } from 'enzyme';
+import App from '../../src/App';
+
+describe('App', () => {
+    it('renders Hello, World!', () => {
+        const wrapper = mount(<App />);
+        const helloClassName = '.hello';
+        expect(wrapper.find(helloClassName).text()).toContain('Hello, world!');
+    });
+});
